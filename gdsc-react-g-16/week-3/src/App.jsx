ac7a7b82c9ba241257b7b1537a9d7ad25293699e
Abdurazakm <abdulkem5472@gmail.com>
2024-12-25 17:39:06 +0300

    combine state in to the div with userCard

--- gdsc-react-g-16/week-3/src/App.jsx
+++ gdsc-react-g-16/week-3/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import UserCard from "./UserCard";
 import State from "./State";
 function App(){
-  // return(
-  //   <div>
-  //     <h1>props in React</h1>
-  //     <UserCard name = {"Abdurazak"} email={"abdurazakm343@gmail.com"} age={21}/>
-  //   </div>
-  // )
   return(
     <div>
+      {/*This from Usercard file to print card which contain name, email and age by using props*/}
+      <h1>props in React</h1>
+      <UserCard name = {"Abdurazak"} email={"abdurazakm343@gmail.com"} age={21}/>
+
+
+      {/* This from State function to print couter by using state function */}
       <State/>
     </div>
   )
