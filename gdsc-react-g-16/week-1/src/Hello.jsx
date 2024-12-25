068b7ad5021ddfadeb20d9fa004e09abfa12fed3
Abdurazakm <abdulkem5472@gmail.com>
2024-12-25 17:16:28 +0300

    second commit

---
+++ gdsc-react-g-16/week-1/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+    return (
+      <>
+        <h1>Hello Abdurazak Welcome To GDG on Compus</h1>
+        <h2>These are your Group member</h2>
+      </>
+    )
+  }
+  export default Hello;
