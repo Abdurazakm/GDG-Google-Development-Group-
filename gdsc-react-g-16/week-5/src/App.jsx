d7bb212350c4613c974b80c53c7301f487c610bd
Abdurazakm <abdulkem5472@gmail.com>
2025-03-05 14:27:52 +0300

    week-5 final

--- gdsc-react-g-16/week-5/src/App.jsx
+++ gdsc-react-g-16/week-5/src/App.jsx
@@ -1,28 +1,38 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
-import Home from "./Home";
-import AboutUs from "./AboutUs";
-import Contact from "./Contact";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
+import AboutUs from "./component/AboutUs";
+import Contact from "./component/Contact";
+import Navbar from "./component/Navbar";
+import Order_summery from "./component/Order_summery";
+import NoMatch from "./component/NoMatch";
+import Products from "./component/Products";
+import Feature from "./component/Feature";
+import New from "./component/New";
+import Users from "./component/Users";
+import UserDetail from "./component/UserDetail";
+import Admin from "./component/admin";
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/home">Home</Link>
-        <Link to="/about">About US</Link>
-        <Link to="/Contact">Contact</Link>
-      </nav>
+    <div>
+      <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/Contact" element = {<Contact />}/>
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/order_summery" element={<Order_summery />} />
+        <Route path="*" element={<NoMatch />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<Feature />} />
+          <Route path="feature" element={<Feature />} />
+          <Route path="new" element={<New />} />
+        </Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetail />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
---
+++ gdsc-react-g-16/week-6/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from "./Hello";
+import Ex1 from "./Ex1";
+
+// Main App component
+function App() {
+  return (
+    <>
+      <div>
+        <Hello /> {/* Render Hello component */}
+        <Ex1 /> {/* Render Ex1 component */}
+      </div>
+    </>
+  );
+}
+
+export default App;
