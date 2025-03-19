2e17f4d0b1ef1440279f225734a900db584e2550
Abdurazakm <abdulkem5472@gmail.com>
2025-03-19 14:48:11 +0300

    week 8 initail commit

---
+++ gdsc-react-g-16/week-8/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LiveSearch from "./components/LiveSearch";
+
+// Main App component
+function App() {
+  return (
+    <>
+      <div>
+       < LiveSearch />
+      </div>
+    </>
+  );
+}
+
+export default App;
