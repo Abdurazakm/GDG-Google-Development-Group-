37551be7c79cf5050334641201a27d092cd799c9
Abdurazakm <abdulkem5472@gmail.com>
2025-04-01 10:45:24 +0300

    final commit

--- gdsc-react-g-16/week-6/src/App.jsx
+++ gdsc-react-g-16/week-6/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { AuthProvider } from "./components/AuthContext";
+import AuthButton from "./components/AuthButton";
+// import Home from "./components/Home";
+import "./App.css";
 
-// Main App component
 function App() {
   return (
-    <>
-      <div>
-
+    <AuthProvider>
+      <div className="app-container">
+        <h1>User Authentication</h1>
+        <AuthButton />
+        {/* <Home /> */}
       </div>
-    </>
+    </AuthProvider>
   );
 }
 
